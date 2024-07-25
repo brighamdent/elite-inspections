59212a5060c241594e41168c947993637fa76f72
brighamdent <brighamdent@gmail.com>
2024-07-25 12:02:17 -0600

    footer responsive

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,28 +6,30 @@ import { faPhone } from "@fortawesome/free-solid-svg-icons";
 
 export default function Footer() {
   return (
-    <div className="bg-royalblue w-full flex flex-col items-center h-80 p-2 justify-around">
-      <div className="flex justify-around items-center">
+    <div className="bg-royalblue w-full flex flex-col lg:flex-row items-center h-80 lg:h-24 p-2 justify-around lg:justify-between">
+      <div className="flex justify-around items-center lg:order-3 lg:mr-12">
         <div className="w-[55px] h-[55px] rounded-[50px] bg-teal p-3 flex items-center justify-center">
           <FontAwesomeIcon icon={faPhone} />
         </div>
         <div className="flex flex-col items-start ml-4">
-          <h2>Contact Us</h2>
-          <h3>businessemail@gmail.com</h3>
-          <h3>555-555-555</h3>
+          <h3 className="lg:text-sm font-bold">Contact Us</h3>
+          <p className="lg:text-sm">businessemail@gmail.com</p>
+          <p className="lg:text-sm">555-555-555</p>
         </div>
       </div>
-      <div className="flex items-center">
+      <div className="flex items-center lg:order-1 lg:ml-12">
         <Image src={plainLogo} alt="" />
         <div className="flex flex-col text-left w-56 ml-4">
-          <p>Licenced with:</p>
-          <p>
+          <p className="lg:text-xs">Licenced with:</p>
+          <p className="lg:text-xs">
             State of Florida Department of Business and Professional Regulation
           </p>
-          <p>LN: HI17145</p>
+          <p className="lg:text-xs">LN: HI17145</p>
         </div>
       </div>
-      <h3>2024 © | Elite Home Inspection Group </h3>
+      <p className="lg:order-2 lg:text-sm">
+        2024 © | Elite Home Inspection Group{" "}
+      </p>
     </div>
   );
 }
