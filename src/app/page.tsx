1ad2bcc091a70bb812427c751480594368143103
brighamdent <brighamdent@gmail.com>
2024-07-25 12:13:05 -0600

    navbar edits

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,7 +14,6 @@ import Footer from "@/components/Footer";
 export default function Home() {
   return (
     <>
-      <Navbar />
       <div className="bg-hero-house relative  lg:bg-darkblue bg-center h-[82vh] text-white overflow-hidden">
         <div className="absolute inset-0 flex flex-col lg:flex-row items-center justify-between pt-8 pb-8 bg-darkblue bg-opacity-80 lg:bg-opacity-0 p-4">
           <div className="w-full lg:ml-12 lg:m-8 flex flex-col justify-between lg:justify-around h-full lg:h-96 lg:items-start">
