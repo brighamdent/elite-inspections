bd5d27b131048747a7876be06c5d9dc19e1601f4
brighamdent <brighamdent@gmail.com>
2024-08-01 10:33:38 -0700

    about

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,17 +1,55 @@
 import Image from "next/image";
 import React from "react";
 import aboutPortrait from "../../../public/about_portrait.jpg";
+import aboutPortrait2 from "../../../public/about_portrait2.jpg";
+import Footer from "@/components/Footer";
 
 export default function page() {
   return (
-    <div className="relative w-20 h-20 overflow-hidden">
-      <Image
-        src={aboutPortrait}
-        alt="About Portrait"
-        layout="fill"
-        objectFit="cover"
-        className=""
-      />
+    <div className="flex flex-col items-center">
+      <div className="flex flex-col lg:flex-row">
+        <div className="flex flex-col items-center">
+          <h1 className="z-10 relative hidden sm:block m-4">About Us</h1>
+          <div className="relative h-[500px] sm:rounded-3xl w-full sm:w-96 overflow-hidden">
+            <h1 className="z-10 relative sm:hidden">About Us</h1>
+            <Image
+              src={aboutPortrait2}
+              alt="About Portrait"
+              layout="fill"
+              objectFit="cover"
+              objectPosition="top"
+              className=""
+            />
+          </div>
+        </div>
+        <div className="p-6">
+          <h2>Who we are</h2>
+          <p className="mb-4">
+            At Elite Home Inspection Group, we are dedicated to providing
+            thorough, reliable, and professional inspection services. With years
+            of experience in the industry, our team of certified inspectors
+            ensures that every inspection is conducted with the highest level of
+            accuracy and integrity.
+          </p>
+          <h2>Our Mission</h2>
+          <p className="mb-4">
+            Our mission is to deliver peace of mind to our clients by offering
+            comprehensive inspection services that meet and exceed industry
+            standards. We strive to be the most trusted inspection company,
+            known for our commitment to quality and customer satisfaction.
+          </p>
+          <h2>Why Choose Us</h2>
+          <p className="mb-4">
+            Certified Professionals: Our inspectors are fully certified and
+            trained to provide the highest quality inspections. Detailed
+            Reports: We provide clear, comprehensive reports to help you
+            understand the condition of your property. Customer Focused: We
+            prioritize your satisfaction and work diligently to address all your
+            inspection needs.
+          </p>
+        </div>
+      </div>
+      <Footer />
     </div>
   );
 }
--- src/components/CallButton.tsx
+++ src/components/CallButton.tsx
@@ -9,7 +9,7 @@ export default function CallButton() {
       className="bg-royalblue h-[70px] w-[275px] lg:h-[60px] lg:w-[250px] rounded-[55px] p-2 flex items-center "
     >
       <div className="flex justify-between items-center w-full">
-        <h2 className="ml-10 lg:ml-8">555-555-555</h2>
+        <h2 className="ml-10 lg:ml-4">555-555-555</h2>
         <div className="w-[55px] h-[55px] lg:w-[50px] lg:h-[50px] rounded-[50px] bg-teal p-2 flex items-center justify-center">
           <FontAwesomeIcon icon={faPhone} className="h-6" />
         </div>
