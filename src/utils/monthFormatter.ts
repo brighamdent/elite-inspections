a599496ebee511d07de530f420800a5fb5b111c3
brighamdent <brighamdent@gmail.com>
2024-08-07 08:23:29 -0700

    calandar variables

--- src/utils/monthFormatter.ts
+++
@@ -1,6 +0,0 @@
-import { format } from "date-fns";
-const monthFormatter = (currentDate: any) => {
-  const monthFormatted = format(currentDate, "MMMM");
-  return monthFormatted;
-};
-export default monthFormatter;
