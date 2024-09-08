2e6f036207e63d885a6e2bc88bba993c2e656935
brighamdent <brighamdent@gmail.com>
2024-09-08 13:35:58 -0700

    pool fix and some css

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -7,6 +7,7 @@ const pool = mysql.createPool({
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   waitForConnections: true,
+  idleTimeoutMillis: 30000,
   connectionLimit: 10,
   queueLimit: 0,
 });
