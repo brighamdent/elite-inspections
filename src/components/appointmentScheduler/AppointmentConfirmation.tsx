2839c84fec5bf9676357c1faa9106289680f6b1d
brighamdent <brighamdent@gmail.com>
2024-09-10 14:21:38 -0700

    responsive design fix for appointment confirmed

--- src/components/appointmentScheduler/AppointmentConfirmation.tsx
+++ src/components/appointmentScheduler/AppointmentConfirmation.tsx
@@ -7,25 +7,25 @@ import SelectedAppointment from "./SelectedAppointment";
 
 export default function AppointmentConfirmation() {
   return (
-    <div className="flex flex-col items-center w-full md:pr-9 md:pl-9">
-      <div className="flex items-center md:self-start">
+    <div className="flex flex-col items-center w-screen md:w-full pr-4 pl-4 md:pr-9 md:pl-9">
+      <div className="flex items-center justify-center md:self-start">
         <h2>Appointment Confirmed</h2>
-        <div className="bg-teal rounded-3xl h-10 w-10 ml-2 flex items-center justify-center ">
+        <div className="bg-teal rounded-3xl h-10 min-w-10 ml-2 flex items-center justify-center ">
           <FontAwesomeIcon icon={faCheck} className="h-6" />
         </div>
       </div>
-      <h3 className=" md:self-start text-center md:text-left md:mr-20 mt-2 w-80 md:w-auto">
+      <h3 className=" md:self-start text-center md:text-left md:mr-20 mt-2 md:w-full md:pr-0 md:pl-0 mb-4 md:mb-0">
         Your apointment has been confirmed! We sent you an email with all of the
         details.{" "}
       </h3>
-      <div className="flex flex-col items-center md:items-start w-full">
+      <div className="flex flex-col items-center md:items-start w-full mb-4 md:md-0">
         <SelectedAppointment edit={false} />
       </div>
       <div className="w-full flex flex-col items-center md:items-stretch">
         <PersonalDetails edit={false} />
         <PropertyDetails edit={false} />
       </div>
-      <p className="md:text-left w-80 md:w-auto">
+      <p className="md:text-left md:w-auto">
         If you need to change any of your appointment details please give us a
         call at 555-555-555 or email us at businessemail@gmail.com
       </p>
